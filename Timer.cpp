d2c5e089d75093cf8a484f71ad205325cefc768c
VasiliiSokolov <vasilysokolov@mail.ru>
2023-03-16 20:53:32 +0300

    fast

--- Timer.cpp
+++ Timer.cpp
@@ -12,21 +12,28 @@ int main()
     std::cout << "Please, put time (mm/ss): ";
     std::cin >> std::get_time(&date, "%M/%S");
 
-    //std::cout << asctime(&date);
+    int distance = date.tm_min*60 + date.tm_sec;
+    /*std::cout << asctime(&date);
     std::tm fin = *std::localtime(&t);
     fin.tm_min += date.tm_min;
     fin.tm_sec += date.tm_sec;
     std::time_t f = std::mktime(&date);
 
-    std::cout << difftime(t, f) << std::endl;
+    std::cout << difftime(t, f) << std::endl;*/
 
-    while (difftime(t, f) > 0)
+    while (true) 
     {
+        
         std::time_t d = std::time(nullptr);
-        std::tm now = *std::localtime(&t);
+        //std::cout << (int)difftime(d, t) / 60 << std::endl;
+        //std::cout << (int)difftime(d, t) % 60 << std::endl;
+        if ((int)difftime(d, t) > distance) break;
+        std::tm now = *std::localtime(&d);
+        now.tm_min = (int)difftime(d, t) / 60;
+        now.tm_sec = (int)difftime(d, t) % 60;
         std::cout << std::put_time(&now, "%M/%S") << std::endl;
-        t += difftime(t, d);
+        
     }
-
+    std::cout << "Ding, ding, ding!" << std::endl;
 }
 
